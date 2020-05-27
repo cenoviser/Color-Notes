2269cd0020630b500a99f4e4fdecddacb32a8d15
Ian Jiwoo Ban <jban365@gmail.com>
2020-05-27 15:21:41 +0900

    Initial Commit

---
+++ RealmNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmNote
+//
+//  Created by Jiwoo Ban on 2020/05/27.
+//  Copyright © 2020 Jiwoo Ban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
