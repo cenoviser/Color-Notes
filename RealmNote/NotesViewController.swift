326535cbcba41b5921bd8bc13fc0daf2b552ae9b
Ian Jiwoo Ban <jban365@gmail.com>
2020-06-03 00:29:10 +0900

    노트 네이게이션바 커스터마이징

--- RealmNote/NotesViewController.swift
+++ RealmNote/NotesViewController.swift
@@ -11,5 +11,14 @@ import UIKit
 class NotesViewController: UIViewController {
     override func viewDidLoad() {
         self.view.backgroundColor = Theme.backgroundColor
+        self.navigationController?.navigationBar.barStyle = .black
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+        self.navigationItem.title = "Realm Notes"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: nil)
+        
+        
+        
     }
 }
